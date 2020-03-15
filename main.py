9945ce284226dc16ebbb33f4656d4fad86894bb0
Gabriel Preite <gabriel@gabriel-Desktop-Deb.lan>
2020-03-15 21:12:17 +0000

    recursive backtracking version

--- main.py
+++ main.py
@@ -1,68 +1,60 @@
 import numpy as np
 import random
+import time
 
+def recursive(x, y):
+    #print("x:"+str(x)+"y:"+str(y))
+    print(visited)
+    time.sleep(0.5)
+    visited[x][y] = 1
+
+    nb = []
+    if(x>0 and not visited[x-1][y]):
+        nb.append([x-1, y])
+    if(y>0 and not visited[x][y-1]):
+        nb.append([x, y-1])
+    if(x<size_x-1 and not visited[x+1][y]):
+        nb.append([x+1, y])
+    if(y<size_y-1 and not visited[x][y+1]):
+        nb.append([x, y+1]) #checks for neighbours
+    
+    while nb:
+        sel = random.randint(-0, len(nb)-1)
+        #print("xs:"+str(nb[sel][0])+"ys:"+str(nb[sel][1]))
+        if not visited[nb[sel][0]][nb[sel][1]]:
+            if(nb[sel][0] == x-1 and nb[sel][1] == y):
+                maze[x][y][0] = 0
+                maze[x-1][y][2] = 0
+            elif(nb[sel][0] == x and nb[sel][1] == y-1):
+                maze[x][y][1] = 0
+                maze[x][y-1][3] = 0
+            elif(nb[sel][0] == x+1 and nb[sel][1] == y):
+                maze[x][y][2] = 0
+                maze[x+1][y][0] = 0
+            elif(nb[sel][0] == x and nb[sel][1] == y+1):
+                maze[x][y][3] = 0
+                maze[x][y+1][1] = 0 #removes walls from current and selected node
+            xs = nb[sel][0]
+            ys = nb[sel][1]
+            visited[nb[sel][0]][nb[sel][1]] = 1 #mark as visited
+        nb.remove([nb[sel][0], nb[sel][1]])
+        for i in range(size_x):
+            for j in range(size_y):
+                print(maze[i][j], end=' ')
+            print("\n")
+        recursive(xs, ys)
+    return
+
+random.seed()
 size_x = 5
 size_y = 4
 
-maze_mask = [[[0 for i in range(2)] for j in range(size_y)] for k in range(size_x)]
-random.seed()
+maze = [[[1 for i in range(4)] for j in range(size_y)] for k in range(size_x)]
+visited = np.zeros([size_x, size_y])
 
-for i in range(size_x): #generates random map
-    for j in range(size_y):
-        print(str(i)+", "+str(j))
-        maze_mask[i][j] = [-1, -1]
-        #if(i>0): #up
-            #maze_mask[i][j][0] = random.randint(1, 100)
-        #if(j>0): #left
-            #maze_mask[i][j][1] = random.randint(1, 100)
-        if(i<size_x-1): #down
-            maze_mask[i][j][0] = random.randint(1, 100)
-        if(j<size_y-1): #right
-            maze_mask[i][j][1] = random.randint(1, 100)
-#maze_mask[0][0][1] = 0
-#maze_mask[size_x-1][size_y-1][3] = 0 #opens entrance and exit
+recursive(0, 0)
 
 for i in range(size_x):
     for j in range(size_y):
-        print(maze_mask[i][j], end=' ')
+        print(maze[i][j], end=' ')
     print("\n")
-
-edges = list()
-
-for i in range(size_x):
-    for j in range(size_y):
-        for k in range(2):
-            if(maze_mask[i][j][k]>0):
-                edges.append([maze_mask[i][j][k], i, j])
-edges.sort() #creates a graph from the mask
-for i in edges:
-    print(i)
-
-"""for i in range(size_x):
-    for j in range(size_y):
-        minim = 6
-        mask = list("0000")
-        if(i>0):
-            minim = min(minim, map[i-1][j])
-        if(j>0):
-            minim = min(minim, map[i][j-1])
-        if(i<size_x-1):
-            minim = min(minim, map[i+1][j])
-        if(j<size_y-1):
-            minim = min(minim, map[i][j+1]) #finds the minimum value
-
-        if(i>0 and map[i-1][j]==minim):
-            mask[0] = "1"
-        if(j>0 and map[i][j-1]==minim):
-            mask[1] = "1"
-        if(i<size_x-1 and map[i+1][j]==minim):
-            mask[2] = "1"
-        if(j<size_y-1 and map[i][j+1]==minim):
-            mask[0] = "1" #connects the maze
-        
-        maze_mask[i][j] = mask
-
-for i in range(size_x):
-    for j in range(size_y):
-        print(maze_mask[i][j], end=' ')
-    print("\n")"""
